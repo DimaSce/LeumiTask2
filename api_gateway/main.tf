096e2fb1eebef43d03b036ed05c56dc2e1127f7b
dmitry.korkin <bonadames@gmail.com>
2023-03-01 12:21:24 +0200

    add terraform lamda&s3 configuration and api_gateway  configuration

---
+++ api_gateway/main.tf
@@ -0,0 +1,62 @@
+
+
+resource "aws_api_gateway_rest_api" "example" {
+  name = "example-api"
+}
+
+resource "aws_api_gateway_resource" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "example"
+}
+
+resource "aws_api_gateway_method" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = "GET"
+  authorization = "NONE" 
+  
+}
+
+
+resource "aws_api_gateway_integration" "example" {
+  rest_api_id             = aws_api_gateway_rest_api.example.id
+  resource_id             = aws_api_gateway_resource.example.id
+  http_method             = aws_api_gateway_method.example.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+resource "aws_lambda_permission" "example" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name =  var.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.example.id}/*/*/example"
+  
+}
+
+resource "aws_api_gateway_method_response" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+  status_code = aws_api_gateway_method_response.example.status_code
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  depends_on  = [aws_api_gateway_integration.example, aws_api_gateway_method_response.example, aws_api_gateway_integration_response.example]
+  stage_name  = "prod"
+}
+
+
+
+
