cae7335159a99925e8a412f6078cc9ba021dafdd
dmitry.korkin <bonadames@gmail.com>
2023-03-02 14:32:52 +0200

    fixed

--- lambda_s3/hello-world/hello.js
+++ lambda_s3/hello-world/hello.js
@@ -3,7 +3,7 @@
 module.exports.handler = async (event) => {
   console.log('Event: ', event);
 
-  let responseMessage = 'Check new message';
+  let responseMessage = 'for you message';
 
 
   return {
